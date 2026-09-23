42f2c09ee1aabef16296b0d9fb75db244335a421
pdahlbeck <dahlbeck@gmail.com>
2026-09-23 12:34:46 +0200

    Bundle SwiftTerm license with app

--- build-app.sh
+++ build-app.sh
@@ -4,6 +4,10 @@ set -eu
 swift build -c release --build-system native
 APP="build/HafthiMac.app"
 mkdir -p "$APP/Contents/MacOS"
+mkdir -p "$APP/Contents/Resources"
 cp ".build/release/HafthiMac" "$APP/Contents/MacOS/HafthiMac"
 cp Info.plist "$APP/Contents/Info.plist"
+if [ -f ".build/checkouts/SwiftTerm/LICENSE" ]; then
+    cp ".build/checkouts/SwiftTerm/LICENSE" "$APP/Contents/Resources/SwiftTerm-LICENSE.txt"
+fi
 printf 'Built %s\n' "$APP"
